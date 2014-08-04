675a0a5284d8fa02daad4923b5f23135834deffc
Layne Thorup <layne@lthorup.com>
2014-08-04 13:11:48 -0300

    Test commit 3 from work

--- Prolog/src/com/lthorup/prolog/Prolog.java
+++ Prolog/src/com/lthorup/prolog/Prolog.java
@@ -3,7 +3,7 @@ package com.lthorup.prolog;
 import java.util.ArrayList;
 
 /*
- * This class implements a simple prolog interpreter
+ * This class implements a very simple prolog interpreter
  */
 public class Prolog implements Runnable {
 
